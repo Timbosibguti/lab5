ffaefb9c4f458a7b9eba82a8be0adf3379e6e9cd
Timbosibguti <farrukh.kudratov.95@mail.ru>
2016-04-27 20:20:14 +0600

    perviy test

--- test/app_test.c
+++ test/app_test.c
@@ -3,5 +3,12 @@
 
 CTEST(app_suite, nuli)
 {
-    //
+    const double a = 0, b = 0, c = 0;
+    double d, x1, x2;
+
+    const double koren = uravn(a, b, c, &d, &x1, &x2);
+    
+    const double ex_koren = -1;
+
+    ASSERT_EQUAL(ex_koren, koren);
 }
